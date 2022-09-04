697bc4d55feabb3be2ab8c3c4f3af5a42789e93f
RoyalPiano <zelyoniyoleg@yandex.ru>
2022-09-04 13:01:18 +0500

    deleted useless state classes

--- app/src/main/java/ru/example/cryptocurrency/presentation/coin_detail/CoinDetailState.kt
+++
@@ -1,9 +0,0 @@
-package ru.example.cryptocurrency.presentation.coin_detail
-
-import ru.example.cryptocurrency.domain.model.Coin
-import ru.example.cryptocurrency.domain.model.CoinDetail
-
-data class CoinDetailState(
-    val isLoading: Boolean = false,
-    val coin: CoinDetail? = null,
-)
--- app/src/main/java/ru/example/cryptocurrency/presentation/coin_list/CoinListState.kt
+++
@@ -1,8 +0,0 @@
-package ru.example.cryptocurrency.presentation.coin_list
-
-import ru.example.cryptocurrency.domain.model.Coin
-
-data class CoinListState(
-    val isLoading: Boolean = false,
-    val coins: List<Coin> = emptyList(),
-)
